b0980a7596d6f2dbe0197d1cce5bd57e9e22e233
banti chowdhury <banti_chy@rediffmail.com>
2017-01-15 21:02:06 -0600

    Fixed caps

--- mysqlselenium/src/main/java/com/mysql/mysqlselenium/SeleniumDatabaseTesting.java
+++ mysqlselenium/src/main/java/com/mysql/mysqlselenium/SeleniumDatabaseTesting.java
@@ -12,6 +12,7 @@ public class SeleniumDatabaseTesting
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!4" );
+        System.out.println( "Fix caps" );
         
         //Testdata
         String email = "abc@gmail.com";
